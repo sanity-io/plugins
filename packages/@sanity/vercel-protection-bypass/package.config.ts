287faca8af0bc7baafb355794b8533fc0894c15e
Cody Olsen <stipsan@gmail.com>
2025-11-20 17:58:49 +0100

    remove cjs

--- packages/@sanity/vercel-protection-bypass/package.config.ts
+++ packages/@sanity/vercel-protection-bypass/package.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from '@sanity/pkg-utils'
 
 export default defineConfig({
-  extract: {
-    rules: {
-      'ae-incompatible-release-tags': 'warn',
-      'ae-internal-missing-underscore': 'off',
-      'ae-missing-release-tag': 'off',
-    },
-  },
+  extract: { enabled: false, },
   strictOptions: {
     noImplicitBrowsersList: 'off',
     noImplicitSideEffects: 'off',
