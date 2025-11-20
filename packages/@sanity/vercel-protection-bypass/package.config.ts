fadc25e067bb8fca4b5a3978cd04b233d090eb2b
Cody Olsen <stipsan@gmail.com>
2025-11-20 14:42:08 +0100

    add `@sanity/vercel-protection-bypass`

---
+++ packages/@sanity/vercel-protection-bypass/package.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "@sanity/pkg-utils";
+
+export default defineConfig({
+  extract: {
+    rules: {
+      "ae-incompatible-release-tags": "warn",
+      "ae-internal-missing-underscore": "off",
+      "ae-missing-release-tag": "off",
+    },
+  },
+  tsconfig: "tsconfig.build.json",
+  babel: { reactCompiler: true },
+  reactCompilerOptions: { target: "18" },
+});
