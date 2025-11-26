8dc2835f3030653ff0e8396821d0c5ad82f0bb3e
Cody Olsen <stipsan@gmail.com>
2025-11-26 10:32:37 +0100

    move graph view to separate workspace

--- dev/test-studio/sanity.config.ts
+++ dev/test-studio/sanity.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'sanity'
+import {defineConfig, type WorkspaceOptions} from 'sanity'
 import {aprimoPlugin} from 'sanity-plugin-aprimo'
 import {contentGraphView} from 'sanity-plugin-graph-view'
 import {workspaceHomeConfig} from 'sanity-plugin-workspace-home'
@@ -15,27 +15,32 @@ import colorInputSchema from './src/color-input'
 const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'ppsg7ml5'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'plugins'
 
-export default defineConfig([
-  workspaceHomeConfig({projectId, dataset}),
-  {
+function createWorkspace(
+  config: Omit<WorkspaceOptions, 'projectId' | 'dataset' | 'basePath'>,
+): WorkspaceOptions {
+  return {
     projectId,
     dataset,
-    basePath: '/kitchen-sink',
+    ...config,
+    basePath: `/${config.name}`,
+  }
+}
+
+export default defineConfig([
+  workspaceHomeConfig({projectId, dataset}),
+  createWorkspace({name: 'content-graph-view', plugins: [contentGraphView()]}),
+  createWorkspace({
     name: 'kitchen-sink',
-    title: 'Kitchen Sink',
     schema: {
       types: [colorInputSchema, aprimoInputSchema],
     },
     plugins: [
       structureTool(),
-      contentGraphView(),
       colorInput(),
       debugSecrets(),
       vercelProtectionBypassTool(),
-      aprimoPlugin({
-        tenantName: 'partner1',
-      }),
+      aprimoPlugin({tenantName: 'partner1'}),
       visionTool(),
     ],
-  },
+  }),
 ])
