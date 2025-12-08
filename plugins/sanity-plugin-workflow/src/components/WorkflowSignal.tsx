cfcc3a7e97906565569b25c8b5f05c6791634769
Cody Olsen <stipsan@gmail.com>
2025-12-08 23:45:20 +0100

    fix: use `getPublishedId` to handle doc id

--- plugins/sanity-plugin-workflow/src/components/WorkflowSignal.tsx
+++ plugins/sanity-plugin-workflow/src/components/WorkflowSignal.tsx
@@ -1,13 +1,12 @@
-import type {ObjectInputProps} from 'sanity'
-
 import {useEffect} from 'react'
+import {type ObjectInputProps, getPublishedId} from 'sanity'
 
 import {useWorkflowContext} from './WorkflowContext'
 
 // This component is loaded at the root level of the Document Form
 // It is used to signal the document ID to the WorkflowProvider
 export default function WorkflowSignal(props: ObjectInputProps) {
-  const documentId = props?.value?.['_id'] ? props.value['_id'].replace(`drafts.`, ``) : null
+  const documentId = props?.value?.['_id'] ? getPublishedId(props.value['_id']) : null
 
   const {addId, removeId} = useWorkflowContext()
 
