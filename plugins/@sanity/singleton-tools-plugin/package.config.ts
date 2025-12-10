7387845dbcc90a2e3a1b4ea8b3b9861d168b0e19
Cody Olsen <stipsan@gmail.com>
2025-12-10 15:46:20 +0100

    reset

--- plugins/@sanity/singleton-tools-plugin/package.config.ts
+++
@@ -1,8 +0,0 @@
-import config from '@repo/package.config'
-import {defineConfig} from '@sanity/pkg-utils'
-
-export default defineConfig({
-  ...config,
-  babel: {reactCompiler: true, styledComponents: true},
-  reactCompilerOptions: {target: '18'},
-})
--- plugins/@sanity/singleton-tools-plugin/src/components/Tool.tsx
+++
@@ -1,3 +0,0 @@
-export default function Tool() {
-  return <>Hello World!</>
-}
