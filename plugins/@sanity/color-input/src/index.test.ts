7745716d4fca9e375bd50489c152b67d3df59759
Cody Olsen <stipsan@gmail.com>
2026-01-30 12:42:07 +0100

    Update index.test.ts

--- plugins/@sanity/color-input/src/index.test.ts
+++ plugins/@sanity/color-input/src/index.test.ts
@@ -8,5 +8,16 @@ test('package exports', {timeout: 30_000}, async () => {
     cwd: fileURLToPath(import.meta.url),
   })
 
-  expect(manifest.exports).toMatchInlineSnapshot()
+  expect(manifest.exports).toMatchInlineSnapshot(`
+    {
+      ".": {
+        "ColorInput": "object",
+        "color": "object",
+        "colorInput": "function",
+        "hslaColor": "object",
+        "hsvaColor": "object",
+        "rgbaColor": "object",
+      },
+    }
+  `)
 })
