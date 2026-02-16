3d7bd663f86ecce4e7878f14cbe20e35e6b20127
pedrobonamin <pedrobonamin@gmail.com>
2026-02-16 09:54:59 +0100

    fix: update tests setup

--- plugins/@sanity/document-internationalization/src/test/setup.ts
+++ plugins/@sanity/document-internationalization/src/test/setup.ts
@@ -1,8 +1,5 @@
 import * as _jestDom from '@testing-library/jest-dom/vitest'
 
-// Prevent unused variable warning - side-effect import that extends vitest expect
-void _jestDom
-
 // Mock window.matchMedia for @sanity/ui components
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
