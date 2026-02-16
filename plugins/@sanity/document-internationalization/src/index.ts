f9719f3faaf30b832cd20da80ebd938b92528792
pedrobonamin <pedrobonamin@gmail.com>
2026-02-16 15:03:59 +0100

    fix: revert removal of deprecated actions

--- plugins/@sanity/document-internationalization/src/index.ts
+++ plugins/@sanity/document-internationalization/src/index.ts
@@ -1,5 +1,13 @@
-export {useDeleteTranslationAction} from './actions/DeleteTranslationAction'
-export {useDuplicateWithTranslationsAction} from './actions/DuplicateWithTranslationsAction'
+export {
+  useDeleteTranslationAction,
+  /* oxlint-disable-next-line typescript-eslint/no-deprecated -- re-exported for backwards compatibility */
+  DeleteTranslationAction,
+} from './actions/DeleteTranslationAction'
+export {
+  useDuplicateWithTranslationsAction,
+  /* oxlint-disable-next-line typescript-eslint/no-deprecated -- re-exported for backwards compatibility */
+  DuplicateWithTranslationsAction,
+} from './actions/DuplicateWithTranslationsAction'
 export {useDocumentInternationalizationContext} from './components/DocumentInternationalizationContext'
 export {DocumentInternationalizationMenu} from './components/DocumentInternationalizationMenu'
 export {documentInternationalization} from './plugin'
