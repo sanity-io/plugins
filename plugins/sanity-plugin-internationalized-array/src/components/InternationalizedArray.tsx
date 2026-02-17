67b54b72ae2954e21ce915b7c1e769860ee5b345
pedrobonamin <pedrobonamin@gmail.com>
2026-02-17 10:28:23 +0100

    fix(i18n-array): support language filter

--- plugins/sanity-plugin-internationalized-array/src/components/InternationalizedArray.tsx
+++ plugins/sanity-plugin-internationalized-array/src/components/InternationalizedArray.tsx
@@ -68,6 +68,7 @@ export default function InternationalizedArray(
   const languageFilterEnabled =
     typeof documentType === 'string' && languageFilterOptions.documentTypes.includes(documentType)
 
+  // TODO:Is this redundant? The filter plugin is already filtering the members, why do we also need to call it at this level.
   const filteredMembers = useMemo(
     () =>
       languageFilterEnabled
@@ -90,10 +91,11 @@ export default function InternationalizedArray(
               member.item.schemaType,
               valueMember,
               selectedLanguageIds,
+              value[member.index],
             )
           })
         : members,
-    [languageFilterEnabled, members, languageFilterOptions, selectedLanguageIds],
+    [languageFilterEnabled, members, languageFilterOptions, selectedLanguageIds, value],
   )
 
   const handleAddLanguages = useCallback(
