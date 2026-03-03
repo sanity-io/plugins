b98596f70bb5105b347d158ec0caa6ebc7c9a0f7
pedrobonamin <pedrobonamin@gmail.com>
2026-03-03 09:06:14 +0100

    fix: extract RemoveButton from input

--- plugins/sanity-plugin-internationalized-array/src/components/InternationalizedInput.tsx
+++ plugins/sanity-plugin-internationalized-array/src/components/InternationalizedInput.tsx
@@ -1,5 +1,6 @@
 import {RemoveCircleIcon} from '@sanity/icons'
 import {
+  Box,
   Button,
   Card,
   Flex,
@@ -70,6 +71,46 @@ function ChangeLanguageButton(props: {
       }
       popover={{portal: true}}
     />
+  )
+}
+
+function RemoveButton({
+  isDefault,
+  readOnly,
+  onChange,
+}: {
+  isDefault: boolean
+  readOnly: boolean
+  onChange: (patch: FormPatch[] | PatchEvent | FormPatch) => void
+}): ReactNode {
+  // Removes this item from the array
+  const handleUnset = (): void => {
+    onChange(unset())
+  }
+
+  return (
+    <Tooltip
+      animate
+      disabled={!isDefault}
+      content={
+        <Text muted size={1}>
+          Can&apos;t remove default language
+        </Text>
+      }
+      fallbackPlacements={['right', 'left']}
+      placement="top"
+      portal
+    >
+      <span>
+        <Button
+          mode="bleed"
+          icon={RemoveCircleIcon}
+          tone="critical"
+          disabled={readOnly || isDefault}
+          onClick={handleUnset}
+        />
+      </span>
+    </Tooltip>
   )
 }
 
@@ -173,19 +214,15 @@ export default function InternationalizedInput(
   const inlineProps = useMemo(
     () => ({
       ...props.inputProps,
-      // This is the magic that makes inline editing work?
       members: props.inputProps.members.filter((m) => m.kind === 'field' && m.name === 'value'),
-      // This just overrides the type
-      // Remove this as it shouldn't be necessary?
-      // oxlint-disable-next-line typescript-eslint/no-unnecessary-type-assertion
-      value: props.value as InternationalizedArrayItem,
+      value: props.value,
       // Use our wrapped onChange handler
       onChange: wrappedOnChange,
     }),
     [props.inputProps, props.value, wrappedOnChange],
   )
 
-  const {validation, value, onChange, readOnly} = inlineProps
+  const {validation, value, readOnly} = inlineProps
 
   // The parent array contains the languages from the plugin config
   const {languages, languageDisplay, defaultLanguages} = useInternationalizedArrayContext()
@@ -193,11 +230,6 @@ export default function InternationalizedInput(
   const keyIsValid = languages?.length
     ? languages.find((l) => l.id === value[LANGUAGE_FIELD_NAME])
     : false
-
-  // Removes this item from the array
-  const handleUnset = (): void => {
-    onChange(unset())
-  }
 
   if (!languages) {
     return <Spinner />
@@ -212,48 +244,20 @@ export default function InternationalizedInput(
   return (
     <Card paddingTop={2} tone={getToneFromValidation(validation)}>
       <Stack space={2}>
-        <Card tone="inherit">
-          {keyIsValid ? (
-            <Label muted size={1}>
-              {languageTitle}
-            </Label>
-          ) : (
-            <ChangeLanguageButton
-              value={value}
-              path={props.path}
-              onChange={props.inputProps.onChange}
-            />
-          )}
-        </Card>
+        {keyIsValid ? (
+          <Label muted size={1}>
+            {languageTitle}
+          </Label>
+        ) : (
+          <ChangeLanguageButton value={value} path={props.path} onChange={originalOnChange} />
+        )}
         <Flex align="center" gap={2}>
-          <Card flex={1} tone="inherit">
-            {props.inputProps.renderInput(inlineProps)}
-          </Card>
-
-          <Card tone="inherit">
-            <Tooltip
-              animate
-              disabled={!isDefault}
-              content={
-                <Text muted size={1}>
-                  Can&apos;t remove default language
-                </Text>
-              }
-              fallbackPlacements={['right', 'left']}
-              placement="top"
-              portal
-            >
-              <span>
-                <Button
-                  mode="bleed"
-                  icon={RemoveCircleIcon}
-                  tone="critical"
-                  disabled={readOnly || isDefault}
-                  onClick={handleUnset}
-                />
-              </span>
-            </Tooltip>
-          </Card>
+          <Box flex={1}>{props.inputProps.renderInput(inlineProps)}</Box>
+          <RemoveButton
+            isDefault={isDefault}
+            readOnly={Boolean(readOnly)}
+            onChange={originalOnChange}
+          />
         </Flex>
       </Stack>
     </Card>
