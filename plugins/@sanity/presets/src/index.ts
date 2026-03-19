cf0299f6c0584797a9936f68bf6c20e390b68a76
Ash <ash@sanity.io>
2026-03-19 14:35:20 +0000

    feat(@sanity/presets): add `pageType`

--- plugins/@sanity/presets/src/index.ts
+++ plugins/@sanity/presets/src/index.ts
@@ -1,6 +1,7 @@
 export {presets} from './composer'
 export {linkType, LINK_TYPE_NAME} from './presets/link-type'
 export {ctaType} from './presets/cta-type'
+export {pageType} from './presets/page-type'
 export {seoType} from './presets/seo-type'
 export type {LinkTypeConfig} from './presets/link-type'
 export type {PresetResult} from './types'
