5711683450b98004cded44e31ab9807e0899b299
Ash <ash@sanity.io>
2026-03-19 14:35:20 +0000

    feat(@sanity/presets): add `ctaType`

--- plugins/@sanity/presets/src/index.ts
+++ plugins/@sanity/presets/src/index.ts
@@ -1,4 +1,5 @@
 export {presets} from './composer'
 export {linkType, LINK_TYPE_NAME} from './presets/link-type'
+export {ctaType} from './presets/cta-type'
 export type {LinkTypeConfig} from './presets/link-type'
 export type {PresetResult} from './types'
