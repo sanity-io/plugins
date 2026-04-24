615b522e0cb08cf901b1d3e2bd787bdc052cb1a1
Ash <ash@sanity.io>
2026-04-24 12:21:14 +0100

    chore(presets): remove unused PresetResult type

--- plugins/@sanity/presets/src/types.ts
+++ plugins/@sanity/presets/src/types.ts
@@ -1,9 +1,4 @@
-import type {DefineSchemaBase, IntrinsicTypeName, PreviewConfig, SchemaTypeDefinition} from 'sanity'
-
-export interface PresetResult {
-  identifier?: string
-  type: SchemaTypeDefinition
-}
+import type {DefineSchemaBase, IntrinsicTypeName, PreviewConfig} from 'sanity'
 
 export type PartialSchemaDefinition<TypeName extends IntrinsicTypeName> = Partial<
   DefineSchemaBase<TypeName, TypeName> & {preview: PreviewConfig}
