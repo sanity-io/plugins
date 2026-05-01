43b39cfdfa531e62adcae8f314352d2b0a1f838f
Ash <ash@sanity.io>
2026-05-01 09:46:27 +0100

    feat(@sanity/presets): require `name` in types

--- plugins/@sanity/presets/src/types.ts
+++ plugins/@sanity/presets/src/types.ts
@@ -2,4 +2,5 @@ import type {DefineSchemaBase, IntrinsicTypeName, PreviewConfig} from 'sanity'
 
 export type PartialSchemaDefinition<TypeName extends IntrinsicTypeName> = Partial<
   DefineSchemaBase<TypeName, TypeName> & {preview: PreviewConfig}
->
+> &
+  Pick<DefineSchemaBase<TypeName, TypeName>, 'name'>
